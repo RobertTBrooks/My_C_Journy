479b87938af1e02d61248b7fc9e7e81cb8fa8cfb
Robert Brooks <robertbrooks1613@outlook.com>
2026-02-06 13:29:52 -0500

    Day 2 of progress so far: completed Projects01-03 so far working on 04 today.

--- first_10_simple_projects/Project03.c
+++ first_10_simple_projects/Project03.c
@@ -17,6 +17,56 @@
 #include <time.h>
 
 int main() {
+    int guess, target;
+    char *guess_result;
+    // run srand() first to start the seed. 
+    // Since time changes every seconds it is a good seed to use with less setup.
+    srand(time(NULL));
+
+    //now that it ran, rand() will look for srand to produce a random number.
+
+    target = (rand() % 100) + 1;
+
+    printf("Pick a number from 1 to 100: ");
+    scanf("%d", &guess);
+
+
+    while (guess != target) {
+
+
+        guess_result = (guess < target) ? "Too low\n" : "Too high\n";
+        
+        /** if (guess < target) {
+             guess_result = "To low\n";
+
+        } else {
+            guess_result = "To high\n";
+
+        } */
+
+        printf(
+            "\n"
+            "Your guess was %s\n"
+            "Guessed number: %d\n",
+            guess_result,
+            guess
+        );
+        printf("Guess again: ");
+        scanf("%d", &guess);
+    
+    }
+
+    if (guess == target) {
+        printf(
+            "\nYour Guess Was Correct!\n"
+            "Your Guess was: %d and the target was %d",
+            guess,
+            target
+        );
+
+    }
+        
+
 
     return 0;
 }
